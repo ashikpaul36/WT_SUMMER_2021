bd4f62e8499e01ac3a6788ca183df21441874d3d
ashikpaul36 <84740644+ashikpaul36@users.noreply.github.com>
2021-06-28 14:43:55 -0700

    Add files via upload

---
+++ Mid Project/Home.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Public Home Page</title>
+</head>
+<body>
+
+	<?php require'Welcome.php'?>
+	
+	<h1>Welcome To Vehicle Rent Management System</h1>
+	
+	<?php  require'footer.php' ?>
+	
+
+</body>
+</html>
